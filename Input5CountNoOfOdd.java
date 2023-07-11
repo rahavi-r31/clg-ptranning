4d78017909cb3731b0982b3592c62f6363278a02
rahavi-r31 <114447213+rahavi-r31@users.noreply.github.com>
2023-07-11 20:49:55 +0530

    Add files via upload

---
+++ Input5CountNoOfOdd.java
@@ -0,0 +1,21 @@
+public class Input5CountNoOfOdd{
+	public int c0(int i1, int i2, int i3, int i4, int i5) {
+		int s=0;
+		if (i1%2!=0){
+            s=s+1;
+        }
+		if (i2%2!=0){
+            s=s+1;
+        }
+		if (i3%2!=0){
+            s=s+1;
+        }
+		if (i4%2!=0){
+            s=s+1;
+        }
+		if (i5%2!=0){
+            s=s+1;
+        }
+		return s;
+	}
+}
